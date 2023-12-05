0ffc76355a1e6df358787ed60f1d0a7a623b26f9
Lucas Fernandes <lf508319@ohio.edu>
2023-12-05 15:20:47 -0500

    added 3 new visualizations, filter data only to 2018 and removed humidity filter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import sqlite3
 
 # STREAMLIT TITLES
@@ -25,6 +26,7 @@ df_results = pd.read_sql(sql_query, conn)
 #SELECT TEAM
 team = st.sidebar.selectbox('Select a team', sorted(list(df_teams['team_id'].drop_duplicates())))
 selected_team_name = list(df_teams[df_teams['team_id'] == team]['team_name'])
+st.subheader(selected_team_name[0])
 
 # SELECT HOST
 host = st.sidebar.selectbox('Select where team is playing', ['Home', 'Away', 'Both'])
@@ -58,21 +60,14 @@ wind_speed = st.sidebar.slider('Select a wind speed range in mph', min_value=int
                                      value=(int(wind_speeds[int(len(wind_speeds)*0.10)]), 
                                             int(wind_speeds[int(len(wind_speeds)*0.90)])), step=1)
 
-# WEATHER HUMIDITY SLIDER
-weather_humidity = sorted(df_results['weather_humidity'].dropna().unique())
-humidity = st.sidebar.slider('Select a humidity range', min_value=int(weather_humidity[0]), 
-                                     max_value=int(weather_humidity[-1]), 
-                                     value=(int(weather_humidity[int(len(weather_humidity)*0.10)]), 
-                                            int(weather_humidity[int(len(weather_humidity)*0.90)])), step=1)
-
 # YEAR SLIDER
-years = sorted(df_results['schedule_season'].dropna().unique())
+years = sorted([1966,2018])
 year = st.sidebar.slider('Select a range of seasons', min_value=int(years[0]), 
                                      max_value=int(years[-1]), 
                                      value=(int(years[0]), 
                                             int(years[-1])), step=1)
 
-#SELECT DATA TO BE DISPLAYED BY PLOTLY CHART
+#SELECT HOME/AWAY
 if host == 'Home':
     team_data = df_results[df_results['team_home'].isin(selected_team_name)]
 elif host == 'Away':
@@ -81,7 +76,7 @@ elif host == 'Both':
     team_home = df_results[df_results['team_home'].isin(selected_team_name)]
     team_away = df_results[df_results['team_away'].isin(selected_team_name)]
     team_data = pd.concat([team_home, team_away], ignore_index=True)
-
+    
 # APPLY FILTERS
 team_data = team_data.join(df_stadiums.set_index('stadium_name'), on='stadium')
 team_data = team_data[
@@ -90,19 +85,58 @@ team_data = team_data[
     (team_data['stadium_capacity'].between(*stadium_capacity)) &
     (team_data['weather_temperature'].between(*temperature)) &
     (team_data['weather_wind_mph'].between(*wind_speed)) &
-    (team_data['weather_humidity'].between(*humidity)) &
     (team_data['schedule_season'].between(*year))
 ]
 
-team_data['Performance'] = team_data.apply(lambda row: row['score_home'] - row['score_away'] if row['team_home'] in selected_team_name else row['score_away'] - row['score_home'], axis=1)
+#GAME BY GAME PERFORMANCE DISPLAY
+team_data['Performance'] = team_data.apply(lambda row: row['score_home'] - row['score_away'] 
+                                           if row['team_home'] in selected_team_name 
+                                           else row['score_away'] - row['score_home'], axis=1)
+team_data['game_result'] = team_data['Performance'].apply(lambda x: 'win' 
+                                                          if x > 0 
+                                                          else ('tie' if x == 0 else 'loss'))
 
-# SORT BY DATE
 team_data['schedule_date'] = pd.to_datetime(team_data['schedule_date'], format='%m/%d/%Y')
 team_data['schedule_date'] = team_data['schedule_date'].dt.date 
 team_data = team_data.sort_values(by='schedule_date')
 
-# DISPLAYING DATA
 fig = px.bar(team_data, x='schedule_date', y='Performance')
+
+st.markdown("Game by game performance with given conditions")
+st.plotly_chart(fig, use_container_width=True)
+
+#SEASON BY SEASON PERFORMANCE DISPLAY
+
+#CALCULATE SEASON PERFORMANCE
+season_stats = team_data.groupby('schedule_season')['game_result'].value_counts().unstack(fill_value=0)
+try:
+    season_stats['win_percentage'] = round((season_stats['win'] / season_stats[['win', 'loss']].sum(axis=1)) * 100, 1)
+except KeyError:
+    season_stats['win_percentage'] = 0
+
+columns_to_select = ['win_percentage', 'win', 'loss']
+if 'tie' in season_stats.columns:
+    columns_to_select.append('tie')
+
+df_season = season_stats[columns_to_select].reset_index()
+df_season.columns = ['Season', 'Win Percentage', 'Win', 'Loss', 'Tie'] if 'tie' in season_stats.columns else ['Season', 'Win Percentage', 'Win', 'Loss']
+
+
+#FIRST DISPLAY
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=df_season['Season'], y=df_season['Win Percentage'],
+                    mode='lines+markers',
+                    name='lines+markers'))
+st.markdown("Season by season win % with given conditions")
+st.plotly_chart(fig, use_container_width=True)
+
+#SECOND DISPLAY
+st.markdown("Observe data inputted on chart")
+st.dataframe(season_stats)
+
+#THIRD DISPLAY
+fig = px.bar(df_season, x="Season", y=["Win", "Loss"])
+st.markdown("Season by season WIN/LOSS visualization")
 st.plotly_chart(fig, use_container_width=True)
 
 conn.close()  # SHOULD BE LAST LINE OF THE PROGRAM, SO WE CAN MAKE DYNAMIC QUERYING
